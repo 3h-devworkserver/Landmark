ba486f59f2f5d35ef3e40ca10569bb94be591531
info@3hammers.com <anil@3hammers.com>
2016-12-30 12:34:24 +0545

    added course detail satatic page

---
+++ resources/views/frontend/course/coursedetail.blade.php
@@ -0,0 +1,18 @@
+@extends('frontend.master')
+@section('content')
+<section class="main-wrapper">
+
+	<div class="course-detail">
+		<div class="page-style-1 bg-image" style="background-image:url('http://192.168.0.50/landmark/public/img/news---events~banner~1.jpg');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-title">Course Detail</h1>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+	</div>
+</section>			
+@stop
